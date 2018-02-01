ebea81c2e09dcea57f493005effb0b28a044d7bd
Javier Martinez <javier@javiscript.es>
2018-02-01 21:26:34 +0100

    feat(index_final): added scrollSteps library functions

--- js/scrollControll.js
+++ js/scrollControll.js
@@ -1,11 +1,13 @@
-var scollSteps = [
-    ['animate', 'rings'],
-    ['animate', 'cobi'],
-    ['move']
-];
+
+var continueScrolling = true;
+var internalStep = 0;
 
 
 jQuery(document).ready(function($) {
+    var actualHash = parseInt(document.location.hash.replace("#", ""));
+    console.log("Hash actual [" + actualHash + "]");
+    console.log("---- InternalStep [" + internalStep + "]");
+
     SCROLL_FULLPAGE_init();
     document.location.hash = 1;
 });
@@ -14,41 +16,74 @@ jQuery(window).on("resize", function () {
     SCROLL_FULLPAGE_init();
 });
 
-jQuery(window).bind('mousewheel', function(event) {
-    var actualHash = parseInt(document.location.hash.replace("#", ""));
-
-
-    if(event.originalEvent.wheelDelta < 0) {
-        //scroll down
-        document.location.hash = actualHash + 1;
-    }else {
-        //scroll up
-        if (actualHash - 1 > 0) {
-            document.location.hash = actualHash - 1;
-        }
-
-    }
-
-    return false;
+jQuery("body").scrollsteps({
+    up: prev,  // callback for the UP step scroll event, all the events are of course optional
+    down: next,  // callback for the DOWN step scroll event
+    left: left,  // callback for the UP step scroll event, all the events are of course optional
+    right: right,  // callback for the DOWN step scroll event
+    transitionDuration: 1000, // Duration of the main transition event, for example page transitions in a fullPage scroller.
+    quietPeriodBetweenTwoScrollEvents: 100, // Increases responsiveness, minimum delay between two quiet periods (no scroll events) to force the transition event if the transitionDuration is not completed.
 });
 
 jQuery(document).on('touchmove', function() { //touchmove works for iOS, I don't know if Android supports it
     jQuery(window).trigger('mousewheel');
 });
 
+jQuery(window).bind('hashchange', function() {
+    console.log(window.location.hash);
+});
 
 
+function prev () {
+    SCROLLCONTROLL_executeMove("up");
+}
 
+function next () {
+    SCROLLCONTROLL_executeMove("down");
+}
+
+function left () {
+
+}
+
+function right () {
+
+}
 
 function SCROLL_FULLPAGE_init () {
-    jQuery("body").addClass("overflow_hidden");
     jQuery("section").height(jQuery(window).height());
 }
 
-function SCROLLCONTROLL_moveScroll() {
-    var moveScroll = true;
+function SCROLLCONTROLL_executeMove (moveTo) {
+    var actualHash = parseInt(document.location.hash.replace("#", ""));
 
-    moveScroll = false;
+   switch(moveTo) {
+       case "up":
+           if (internalStep - 1 >= 0) {
+               internalStep = internalStep - 1;
+           }
+       break;
 
-    return moveScroll;
+       case "down":
+           internalStep = internalStep + 1;
+       break;
+    }
+
+    console.log("Hash actual [" + actualHash + "]");
+    console.log("---- InternalStep [" + internalStep + "]");
+
+
+    switch (actualHash) {
+        case 1:
+            switch (internalStep) {
+                case 0:
+                    jQuery(".cobi").switchClass("animate", "no-animate", 500, "easeInOutQuad");
+                break;
+                case 1:
+                    jQuery(".cobi").switchClass("no-animate", "animate", 500, "easeInOutQuad");
+                break;
+            }
+        break;
+
+    }
 }
